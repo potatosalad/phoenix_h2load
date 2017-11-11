8764f0da17c4523eebb92557b760b8e439d13b6f
Andrew Bennett <potatosaladx@gmail.com>
2017-11-11 13:46:26 -0700

    Only build Ace handler if present.

--- lib/phoenix_h2load/ace_handler.ex
+++ lib/phoenix_h2load/ace_handler.ex
@@ -1,16 +1,24 @@
-defmodule PhoenixH2load.AceHandler do
-  use Raxx.Server
+case System.get_env("COWBOY_VERSION") do
+  "1" <> _ ->
+    defmodule PhoenixH2load.AceHandler do
+    end
+  "2-pre" <> _ ->
+    defmodule PhoenixH2load.AceHandler do
+    end
+  _ ->
+    defmodule PhoenixH2load.AceHandler do
+      use Raxx.Server
 
-  @impl Raxx.Server
-  def handle_request(_request, _opts) do
-    Raxx.response(:ok)
-    |> Raxx.set_header("content-type", "text/plain")
-    |> Raxx.set_body("Hello world!")
-  end
+      @impl Raxx.Server
+      def handle_request(_request, _opts) do
+        Raxx.response(:ok)
+        |> Raxx.set_header("content-type", "text/plain")
+        |> Raxx.set_body("Hello world!")
+      end
 
-  @impl Raxx.Server
-  def handle_tail(_headers, {request, body, state}) do
-    handle_request(%{request | body: body}, state)
-  end
-
+      @impl Raxx.Server
+      def handle_tail(_headers, {request, body, state}) do
+        handle_request(%{request | body: body}, state)
+      end
+    end
 end
